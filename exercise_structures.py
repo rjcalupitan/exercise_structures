49442360de854ec76aa6d49a04e89c04f53dc6bc
Rhon Joshua Calupitan <35456686+rjcalupitan@users.noreply.github.com>
2018-04-17 22:27:44 +0800

    Add files via upload

---
+++ exercise_structures.py
@@ -0,0 +1,7 @@
+let = ['R', 'H', 'O', 'N', 'J', 'O', 'S', 'H', 'U', 'A', 'C', 'A', 'L', 'U', 'P', 'I', 'T', 'A', 'N']
+
+from collections import Counter
+letters_counts = Counter(letters)
+repeat = let_counts.most_common(5)
+print ("LETTERS DUPLICATED")
+print(repeat)
